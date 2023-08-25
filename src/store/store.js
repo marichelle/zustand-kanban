340f2ed2bbd57c96876461e849cc56942dc704be
Maya Estalilla <mestalilla@gmail.com>
2023-08-25 13:42:07 -0400

    Add devtools middleware to use Redux DevTools

--- src/store/store.js
+++ src/store/store.js
@@ -1,38 +1,63 @@
 import { create } from 'zustand'
+import { devtools } from 'zustand/middleware'
+
+/*
+ * The set function has a second argument, false by default.
+ * Instead of merging, it will replace the state model.
+ *
+ * You can log a specific action type for each set function
+ * by passing a label as the third parameter.
+ */
 
 const store = set => ({
   addTask: (title, status) =>
-    set(prevStore => ({
-      tasks: [
-        ...prevStore.tasks,
-        {
-          id: crypto.randomUUID(),
-          title,
-          status,
-        },
-      ],
-    })),
+    set(
+      prevStore => ({
+        tasks: [
+          ...prevStore.tasks,
+          {
+            id: crypto.randomUUID(),
+            title,
+            status,
+          },
+        ],
+      }),
+      false,
+      'addTask'
+    ),
   deleteTask: id =>
-    set(prevStore => ({
-      tasks: prevStore.tasks.filter(task => task.id !== id),
-    })),
+    set(
+      prevStore => ({
+        tasks: prevStore.tasks.filter(task => task.id !== id),
+      }),
+      false,
+      'deleteTask'
+    ),
   draggedTask: null,
   moveTask: (id, status) =>
-    set(prevStore => ({
-      tasks: prevStore.tasks.map(task =>
-        task.id === id
-          ? {
-              ...task,
-              status,
-            }
-          : task
-      ),
-    })),
+    set(
+      prevStore => ({
+        tasks: prevStore.tasks.map(task =>
+          task.id === id
+            ? {
+                ...task,
+                status,
+              }
+            : task
+        ),
+      }),
+      false,
+      'moveTask'
+    ),
   setDraggedTask: id =>
-    set({
-      draggedTask: id,
-    }),
+    set(
+      {
+        draggedTask: id,
+      },
+      false,
+      'setDraggedTask'
+    ),
   tasks: [],
 })
 
-export const useStore = create(store)
+export const useStore = create(devtools(store))
